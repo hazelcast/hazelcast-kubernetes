8386336499d445230b96766234c16873bd15b979
mrahbar <mrahbar.azad@gmail.com>
2017-11-09 12:40:46 +0100

    added testcase NPE check

--- src/test/java/com/hazelcast/kubernetes/ServiceEndpointResolverTest.java
+++ src/test/java/com/hazelcast/kubernetes/ServiceEndpointResolverTest.java
@@ -84,6 +84,18 @@ public class ServiceEndpointResolverTest {
     }
 
     @Test
+    public void resolveWithNamespaceAndNodeInNamespaceWithNoReadyPods() {
+        Endpoints discoveryNode = createEndpoints(1);
+        discoveryNode.getSubsets().get(0).setAddresses(null);
+        nodesInNamespace.getItems().add(discoveryNode);
+
+        ServiceEndpointResolver sut = new ServiceEndpointResolver(LOGGER, SERVICE_NAME, null, null, NAMESPACE, KUBERNETES_MASTER_URL, API_TOKEN);
+        List<DiscoveryNode> nodes = sut.resolve();
+
+        assertEquals(0, nodes.size());
+    }
+
+    @Test
     @Ignore //enable when https://github.com/hazelcast/hazelcast-kubernetes/pull/30 is merged.
     public void resolveWithServiceLabelAndNodeInNamespace() {
         nodesInNamespace.getItems().add(createEndpoints(1));
